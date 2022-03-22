3870ee8280afee70a5229842dd5dc959e80cdbd3
Muriel Damazio <murieldps@gmail.com>
2022-03-22 19:54:40 -0300

    fix: images on vercel

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -3,37 +3,37 @@ import { ArrowLeft, ArrowRight, GalleryContainer } from './styles'
 const images = [
    {
      id: 1,
-     src: "../../../assets/mars-img1.svg",
+     src: "/assets/mars-img1.svg",
      name: "Imagem 1"
    },
    {
     id: 2,
-    src: "../../../assets/mars-img2.svg",
+    src: "/assets/mars-img2.svg",
     name: "Imagem 2"
    },
    {
     id: 3,
-    src: "../../../assets/mars-img3.svg",
+    src: "/assets/mars-img3.svg",
     name: "Imagem 3"
    },
    {
      id: 4,
-     src: "../../../assets/mars-img1.svg",
+     src: "/assets/mars-img1.svg",
      name: "Imagem 1"
    },
    {
     id: 5,
-    src: "../../../assets/mars-img2.svg",
+    src: "/assets/mars-img2.svg",
     name: "Imagem 2"
    },
    {
      id: 6,
-     src: "../../../assets/mars-img3.svg",
+     src: "/assets/mars-img3.svg",
      name: "Imagem 3"
    },
    {
      id: 7,
-     src: "../../../assets/mars-img1.svg",
+     src: "/assets/mars-img1.svg",
      name: "Imagem 1"
    },
  ]
